017deff1355b71613b42caa54c35b17b99ce89c2
Nicolò Maria Mezzopera <nico@pulilab.com>
2019-06-19 20:09:27 +0200

    feat: add new unicef fields

--- frontend/components/project/sections/ImplementationOverview.vue
+++ frontend/components/project/sections/ImplementationOverview.vue
@@ -9,115 +9,157 @@
       show-legend
     >
       <custom-required-form-item
-        :error="errors.first('health_focus_areas')"
-        :draft-rule="draftRules.health_focus_areas"
-        :publish-rule="publishRules.health_focus_areas"
+        :error="errors.first('result_area')"
+        :draft-rule="draftRules.result_area"
+        :publish-rule="publishRules.result_area"
       >
         <template slot="label">
-          <translate key="health-focus-areas">
-            What is the health focus area(s) addressed by the DHI?
+          <translate key="result-area">
+            UNICEF Result Area
           </translate>
         </template>
 
-        <health-focus-areas-selector
-          v-model="health_focus_areas"
-          v-validate="rules.health_focus_areas"
-          data-vv-name="health_focus_areas"
+        <ResultAreasSelector
+          v-model="result_area"
+          v-validate="rules.result_area"
+          data-vv-name="result_area"
           data-vv-validate-on="change"
-          data-vv-as="Health focus areas"
+          data-vv-as="UNICEF Result Area"
+        />
+      </custom-required-form-item>
+      <custom-required-form-item
+        :error="errors.first('goal_area')"
+        :draft-rule="draftRules.goal_area"
+        :publish-rule="publishRules.goal_area"
+      >
+        <template slot="label">
+          <translate key="unicef-goal">
+            UNICEF Goal
+          </translate>
+        </template>
+
+        <GoalAreasSelector
+          v-model="goal_area"
+          v-validate="rules.goal_area"
+          data-vv-name="goal_area"
+          data-vv-validate-on="change"
+          data-vv-as="UNICEF Goal"
         />
       </custom-required-form-item>
 
-      <custom-required-form-item
-        :error="errors.first('hsc_challenges')"
-        :draft-rule="draftRules.hsc_challenges"
-        :publish-rule="publishRules.hsc_challenges"
-      >
-        <template slot="label">
-          <translate key="hsc-challenges">
-            What are the Health System Challenges addressed by the Digital Health Intervention?
-          </translate>
-        </template>
-        <health-system-challenges-selector
-          v-model="hsc_challenges"
-          v-validate="rules.hsc_challenges"
-          data-vv-name="hsc_challenges"
-          data-vv-validate-on="change"
-          data-vv-as="Health system challenges"
-        />
-      </custom-required-form-item>
-
-      <custom-required-form-item
-        :error="errors.first('platforms')"
-      >
-        <template slot="label">
-          <translate key="platforms">
-            Add information about your Digital Health program activies
-          </translate>
-          <form-hint>
-            <translate key="platforms-hint">
-              Include all software that is part of your project. If you cannot find your software listed in the options, send an email to digitalhealthatlas@gmail.com with the software name.
-            </translate>
-          </form-hint>
-        </template>
-
+      <template v-if="shoDHAFields">
         <custom-required-form-item
-          v-for="(platform, index) in platforms"
-          :key="platform"
-          :error="errors.first('id', 'platform_' + index)"
-          :draft-rule="draftRules.platforms"
-          :publish-rule="publishRules.platforms"
-          class="ItemIndent"
+          :error="errors.first('health_focus_areas')"
+          :draft-rule="draftRules.health_focus_areas"
+          :publish-rule="publishRules.health_focus_areas"
         >
           <template slot="label">
-            <translate key="platform-label">
-              What are the names of the software included in the deployment?
+            <translate key="health-focus-areas">
+              What is the health focus area(s) addressed by the DHI?
             </translate>
           </template>
 
-          <el-col :span="16">
-            <platform-selector
-              :key="platform"
-              v-model="platforms"
-              v-validate="rules.platforms"
-              :data-vv-scope="'platform_' + index"
-              :index="index"
-              data-vv-name="id"
-              data-vv-as="Software"
-            />
-            <custom-required-form-item
-              v-show="platform"
-              :error="errors.first('strategies', 'platform_' + index)"
-              :draft-rule="draftRules.strategies"
-              :publish-rule="publishRules.strategies"
-              class="DigitalHealthIntervention"
-            >
-              <template slot="label">
-                <translate key="strategies">
-                  What Digital Health Intervention(s) are included in this software?
-                </translate>
-                <form-hint>
-                  <!-- This is going to be a link to a pdf / webpage -->
-                </form-hint>
-              </template>
-              <digital-health-interventions-selector
-                v-validate="rules.strategies"
-                :platform-id="platform"
-                :data-vv-scope="'platform_' + index"
-                data-vv-name="strategies"
-                data-vv-as="Digital health interventions"
-              />
-            </custom-required-form-item>
-          </el-col>
-          <el-col :span="8">
-            <add-rm-buttons
-              :show-add="isLastAndExist(platforms, index)"
-              :show-rm="platforms.length > 1"
-              @add="addDhi"
-              @rm="rmDhi(index, platform)"
-            />
-          </el-col>
+          <health-focus-areas-selector
+            v-model="health_focus_areas"
+            v-validate="rules.health_focus_areas"
+            data-vv-name="health_focus_areas"
+            data-vv-validate-on="change"
+            data-vv-as="Health focus areas"
+          />
         </custom-required-form-item>
+
+        <custom-required-form-item
+          :error="errors.first('hsc_challenges')"
+          :draft-rule="draftRules.hsc_challenges"
+          :publish-rule="publishRules.hsc_challenges"
+        >
+          <template slot="label">
+            <translate key="hsc-challenges">
+              What are the Health System Challenges addressed by the Digital Health Intervention?
+            </translate>
+          </template>
+          <health-system-challenges-selector
+            v-model="hsc_challenges"
+            v-validate="rules.hsc_challenges"
+            data-vv-name="hsc_challenges"
+            data-vv-validate-on="change"
+            data-vv-as="Health system challenges"
+          />
+        </custom-required-form-item>
+
+        <custom-required-form-item
+          :error="errors.first('strategies')"
+          :draft-rule="draftRules.strategies"
+          :publish-rule="publishRules.strategies"
+          class="DigitalHealthIntervention"
+        >
+          <template slot="label">
+            <translate key="strategies">
+              Digital Health Intervention(s)
+            </translate>
+            <form-hint>
+              Form Hint
+            </form-hint>
+          </template>
+          <digital-health-interventions-selector
+            v-validate="rules.strategies"
+            :data-vv-scope="'strategies'"
+            data-vv-name="strategies"
+            data-vv-as="Digital health interventions"
+          />
+        </custom-required-form-item>
+      </template>
+
+      <template v-else-if="goal_area">
+        <custom-required-form-item>
+          <template slot="label">
+            {{ selectedGoalArea.capability_level_question }}
+          </template>
+          <CapabilitySelector
+            v-model="capability_levels"
+            :goal-area="goal_area"
+            :values-function="getCapabilityLevelsItems"
+          />
+        </custom-required-form-item>
+        <custom-required-form-item>
+          <template slot="label">
+            {{ selectedGoalArea.capability_category_question }}
+          </template>
+          <CapabilitySelector
+            v-model="capability_categories"
+            :goal-area="goal_area"
+            :values-function="getCapabilityCategoriesItems"
+          />
+        </custom-required-form-item>
+        <custom-required-form-item>
+          <template slot="label">
+            {{ selectedGoalArea.capability_subcategory_question }}
+          </template>
+          <CapabilitySelector
+            v-model="capability_subcategories"
+            :goal-area="goal_area"
+            :values-function="getCapabilitySubcategoriesItems"
+          />
+        </custom-required-form-item>
+      </template>
+
+      <custom-required-form-item
+        :error="errors.first('platforms')"
+        :draft-rule="draftRules.platforms"
+        :publish-rule="publishRules.platforms"
+      >
+        <template slot="label">
+          <translate key="platform-label">
+            What are the names of the software included in the deployment?
+          </translate>
+        </template>
+
+        <platform-selector
+          v-model="platforms"
+          v-validate="rules.platforms"
+          data-vv-name="platforms"
+          data-vv-as="Software"
+        />
       </custom-required-form-item>
 
       <custom-required-form-item
@@ -154,57 +196,60 @@ import ProjectFieldsetMixin from '../../mixins/ProjectFieldsetMixin.js';
 import CollapsibleCard from '../CollapsibleCard';
 import HealthSystemChallengesSelector from '../HealthSystemChallengesSelector';
 import HealthFocusAreasSelector from '../HealthFocusAreasSelector';
+import GoalAreasSelector from '../GoalAreasSelector';
+import ResultAreasSelector from '../ResultAreasSelector';
+import CapabilitySelector from '../CapabilitySelector';
 import PlatformSelector from '../PlatformSelector';
 import DigitalHealthInterventionsSelector from '../DigitalHealthInterventionsSelector';
-import AddRmButtons from '../AddRmButtons';
 import DonorSelector from '../DonorSelector';
 import FormHint from '../FormHint';
 
 import { mapGettersActions } from '../../../utilities/form';
+import { mapGetters } from 'vuex';
 
 export default {
   components: {
     CollapsibleCard,
     HealthSystemChallengesSelector,
     HealthFocusAreasSelector,
+    GoalAreasSelector,
+    ResultAreasSelector,
+    CapabilitySelector,
     PlatformSelector,
     DigitalHealthInterventionsSelector,
-    AddRmButtons,
     DonorSelector,
     FormHint
   },
   mixins: [VeeValidationMixin, ProjectFieldsetMixin],
 
   computed: {
+    ...mapGetters({
+      selectedGoalArea: 'project/getGoalAreaDetails',
+      getCapabilityLevelsItems: 'projects/getCapabilityLevels',
+      getCapabilityCategoriesItems: 'projects/getCapabilityCategories',
+      getCapabilitySubcategoriesItems: 'projects/getCapabilitySubcategories'
+    }),
     ...mapGettersActions({
+      goal_area: ['project', 'getGoalArea', 'setGoalArea', 0],
+      result_area: ['project', 'getResultArea', 'setResultArea', 0],
+      capability_levels: ['project', 'getCapabilityLevels', 'setCapabilityLevels', 0],
+      capability_categories: ['project', 'getCapabilityCategories', 'setCapabilityCategories', 0],
+      capability_subcategories: ['project', 'getCapabilitySubcategories', 'setCapabilitySubcategories', 0],
       platforms: ['project', 'getPlatforms', 'setPlatforms', 0],
       digitalHealthInterventions: ['project', 'getDigitalHealthInterventions', 'setDigitalHealthInterventions', 0],
       health_focus_areas: ['project', 'getHealthFocusAreas', 'setHealthFocusAreas', 0],
       hsc_challenges: ['project', 'getHscChallenges', 'setHscChallenges', 0],
       donors: ['project', 'getDonors', 'setDonors', 0]
-    })
+    }),
+    shoDHAFields () {
+      return this.goal_area === 1;
+    }
   },
   methods: {
-    isLastAndExist (collection, index) {
-      return !!(collection.length - 1 === index && collection[index]);
-    },
-    addDhi () {
-      this.platforms = [...this.platforms, null];
-    },
-    rmDhi (index, platformId) {
-      if (platformId) {
-        const filtered = this.digitalHealthInterventions.filter(dhi => dhi.platform !== platformId);
-        this.digitalHealthInterventions = filtered;
-      }
-      this.platforms = this.platforms.filter((p, i) => i !== index);
-    },
     async validate () {
       this.$refs.collapsible.expandCard();
       const validations = await Promise.all([
-        this.$validator.validate(),
-        this.coverageType === 2
-          ? this.$refs.nationalLevelDeployment.validate()
-          : this.$refs.subNationalLevelDeployment.validate()
+        this.$validator.validate()
       ]);
       console.log('Implementation overview validations', validations);
       return validations.reduce((a, c) => a && c, true);
