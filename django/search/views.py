927fa10085fa5cd2e1ff09752baa02de94575bd2
Gyorgy Fekete <f@pulilab.com>
2019-06-20 09:45:01 +0200

    get rid of unneeded fields on views and serializers for search

--- django/search/views.py
+++ django/search/views.py
@@ -44,9 +44,6 @@ class SearchViewSet(mixins.ListModelMixin, GenericViewSet):
         "project__name",
         "organisation_id",
         "country_id",
-        "project__data__coverage",
-        "project__data__national_level_deployment",
-        "project__data__government_investor",
         "project__approval__approved",
     )
     list_values = (
@@ -54,22 +51,18 @@ class SearchViewSet(mixins.ListModelMixin, GenericViewSet):
         "project__name",
         "organisation_id",
         "country_id",
-        "project__data__geographic_scope",
         "project__data__implementation_overview",
         "project__data__contact_name",
         "project__data__contact_email",
         "project__data__platforms",
         "project__data__health_focus_areas",
         "project__data__hsc_challenges",
-        "project__data__his_bucket",
         "country__region",
-        "project__data__government_investor",
         "donors",
         "project__approval__approved"
     )
     filter_backends = (filters.OrderingFilter,)
-    ordering_fields = ('project__name', 'organisation__name', 'country__name', 'country__region',
-                       'project__data__government_investor', )
+    ordering_fields = ('project__name', 'organisation__name', 'country__name', 'country__region')
     ordering = ('project_id',)
     pagination_class = ResultsSetPagination
 
