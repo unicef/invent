788ce54da6ebd1c103cd357959eefa86ec544655
Nicolò Maria Mezzopera <nico@pulilab.com>
2019-06-20 10:13:26 +0200

    feat: DHIList component

---
+++ frontend/components/project/DhiList.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="DHIList">
+    <ul>
+      <li
+        v-for="p in selected"
+        :key="p.id"
+      >
+        <span>
+          {{ p.name }}
+        </span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  components: {
+  },
+  props: {
+    values: {
+      type: Array,
+      default: () => []
+    }
+  },
+  computed: {
+    ...mapGetters({
+      dhis: 'projects/getDigitalHealthInterventions'
+    }),
+    flattened () {
+      return this.dhis.reduce((a, c) => {
+        const inner = c.subGroups.reduce((innerA, innerC) => {
+          return innerA.concat(innerC.strategies);
+        }, []);
+        return a.concat(inner);
+      }, []);
+    },
+    selected () {
+      return this.flattened.filter(tp => this.values.includes(tp.id));
+    }
+  }
+};
+</script>
+
+<style lang="less">
+  @import "~assets/style/variables.less";
+  @import "~assets/style/mixins.less";
+
+  .DHIList {
+    width: 100%;
+  }
+</style>
