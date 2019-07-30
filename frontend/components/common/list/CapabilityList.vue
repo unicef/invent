35d85e7b80861d21f382a7a2ddac17523ba7c1ff
Nicolò Maria Mezzopera <nicolo.mezzopera@gmial.com>
2019-07-30 14:11:34 +0200

    feat: capability list component

---
+++ frontend/components/common/list/CapabilityList.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="HealthFocusAreasList">
+    <ul>
+      <li
+        v-for="item in selected"
+        :key="item.id"
+      >
+        <list-action
+          v-if="actions"
+          @click="$emit('delete', item.id)"
+        />
+        <span> {{ item.name }} </span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import ListAction from './ListAction';
+import { mapGetters } from 'vuex';
+export default {
+  components: {
+    ListAction
+  },
+  props: {
+    value: {
+      type: Array,
+      default: () => []
+    },
+    actions: {
+      type: Boolean,
+      default: false
+    },
+    limit: {
+      type: Number,
+      default: null
+    },
+    goalArea: {
+      type: Number,
+      required: true
+    },
+    type: {
+      type: String,
+      required: true,
+      validator: value => ['capabilityLevels', 'capabilityCategories', 'capabilitySubcategories'].includes(value)
+    }
+  },
+  computed: {
+    ...mapGetters({
+      capabilityLevels: 'projects/getCapabilityLevels',
+      capabilityCategories: 'projects/getCapabilityCategories',
+      capabilitySubcategories: 'projects/getCapabilitySubcategories'
+    }),
+    items () {
+      return this[this.type](this.goalArea);
+    },
+    selected () {
+      const result = this.items.filter(h => this.value.includes(h.id));
+      return this.limit ? result.slice(0, this.limit) : result;
+    }
+  }
+};
+</script>
+
+<style lang="less">
+.HealthFocusAreasList {
+  width: 100%;
+}
+.HealthFocusAreasSelectorDropdown {
+
+}
+</style>
