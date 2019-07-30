4df274740b90d78a362f619d5d7c53788c0bbf0b
Nicolò Maria Mezzopera <nicolo.mezzopera@gmial.com>
2019-07-30 14:44:32 +0200

    feat: remove unavailable search stuff

--- frontend/utilities/map.js
+++ frontend/utilities/map.js
@@ -1,6 +1,6 @@
 import qs from 'qs';
 
-export const searchIn = () => ['name', 'org', 'country', 'overview', 'partner', 'donor', 'loc'];
+export const searchIn = () => ['name', 'country', 'overview', 'partner', 'donor'];
 
 export const stateGenerator = () => ({
   selectedCountry: null,
