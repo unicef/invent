38a530612dc607eec51a1740b88bd2074787109b
Nicolò Maria Mezzopera <nicolo.mezzopera@gmial.com>
2019-07-31 16:48:58 +0200

    feat: add correct columns to main table / remove unused ones

---
+++ frontend/components/dashboard/GoalAreaItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="GoalAreaItem">
+    {{ selected }}
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  props: {
+    value: {
+      type: Number,
+      default: null
+    }
+  },
+  computed: {
+    ...mapGetters({
+      items: 'projects/getGoalAreas'
+    }),
+    selected () {
+      const found = this.items.find(i => i.id === this.value);
+      return found ? found.name : 'N/A';
+    }
+  }
+};
+</script>
+
+<style lang="less">
+.GoalAreaItem {
+  width: 100%;
+}
+</style>
