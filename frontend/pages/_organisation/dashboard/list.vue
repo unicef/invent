77c34629abaa8577200ffe4c9f192f1ae6971ca8
Nicolò Maria Mezzopera <nicolo.mezzopera@gmial.com>
2019-08-01 09:50:50 +0200

    feat: properly load donor and country details for custom questions

--- frontend/pages/_organisation/dashboard/list.vue
+++ frontend/pages/_organisation/dashboard/list.vue
@@ -39,6 +39,8 @@ export default {
     }
     if (store.getters['dashboard/getDashboardType'] === 'donor') {
       await store.dispatch('system/loadDonorDetails', store.getters['dashboard/getDashboardId']);
+    } else if (store.getters['dashboard/getDashboardType'] === 'country') {
+      await store.dispatch('countries/loadCountryDetails', store.getters['dashboard/getDashboardId']);
     }
   },
   computed: {
