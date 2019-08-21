49cae2b53cf0ea7b7190558be8c6e84e7d3340f2
Nicolò Maria Mezzopera <nicolo.mezzopera@gmial.com>
2019-08-21 10:14:11 +0200

    chore: update domain in letsencrypt

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(tpp.pulilab.com)
+domains=(*.tipp.pulilab.com tipp.pulilab.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="hilive@pulilab.com" # Adding a valid address is strongly recommended
