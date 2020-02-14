3362e6728933bb53697672bf67b0a7287add1ec2
Barnabas <bh@pulilab.com>
2020-02-14 11:28:49 +0100

    remove field_office FK

--- django/project/models.py
+++ django/project/models.py
@@ -60,7 +60,6 @@ class Project(SoftDeleteModel, ExtendedModel):
     odk_id = models.CharField(null=True, blank=True, max_length=64)
     odk_extra_data = JSONField(default=dict)
     country_office = models.ForeignKey('country.CountryOffice', blank=True, null=True, on_delete=models.CASCADE)
-    field_office = models.ForeignKey('country.FieldOffice', blank=True, null=True, on_delete=models.CASCADE)
 
     projects = ProjectManager  # deprecated, use objects instead
     objects = ProjectQuerySet.as_manager()
