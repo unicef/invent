a1d04b11c658b701060d89150cfeaa28d15c2361
Barnabas <bh@pulilab.com>
2020-02-18 13:06:14 +0100

    using send_mail_wrapper

--- django/user/adapters.py
+++ django/user/adapters.py
@@ -1,12 +1,10 @@
 from allauth.account.adapter import DefaultAccountAdapter
 from django.conf import settings
 from django.contrib.sites.models import Site
-from django.core.mail import send_mail
-from django.template import loader
-from django.utils.translation import ugettext, override
 
 # This has to stay here to use the proper celery instance with the djcelery_email package
 import scheduler.celery # noqa
+from core.utils import send_mail_wrapper
 
 
 class DefaultAccountAdapterCustom(DefaultAccountAdapter):
@@ -15,18 +13,11 @@ class DefaultAccountAdapterCustom(DefaultAccountAdapter):
     def send_mail(self, template_prefix, email, context):
         activate_url = self.ACTIVATE_BASE_URL + \
             'en/-/email-confirmation/' + context['key']
-        profile = context['user'].userprofile
-        html_template = loader.get_template("email/master-inline.html")
 
-        with override(profile.language):
-            html_message = html_template.render({"type": "email_confirmation_signup_message",
-                                                 "language": profile.language,
-                                                 "activate_url": activate_url})
-            subject = ugettext("Welcome to the Digital Health Atlas")
+        context.update({"activate_url": activate_url})
 
-        send_mail(
-            subject=subject,
-            message="",
-            from_email=settings.FROM_EMAIL,
-            recipient_list=[email],
-            html_message=html_message)
+        send_mail_wrapper(subject="Welcome to the Digital Health Atlas",
+                          email_type='email_confirmation_signup_message',
+                          to=email,
+                          language=context['user'].userprofile.language,
+                          context=context)
