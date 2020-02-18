091cc3d15f5f13cfe1463c623fde15e2ddef843f
Barnabas <bh@pulilab.com>
2020-02-18 13:05:41 +0100

    using send_mail_wrapper

--- django/toolkit/tasks.py
+++ django/toolkit/tasks.py
@@ -1,12 +1,10 @@
 from datetime import timedelta
 from django.conf import settings
-from django.core.mail import send_mail
 from django.utils import timezone
-from django.utils.translation import ugettext, override
 
-from django.template import loader
 from celery.utils.log import get_task_logger
 
+from core.utils import send_mail_wrapper
 from project.models import Project
 from toolkit.models import Toolkit
 from scheduler.celery import app
@@ -24,17 +22,10 @@ def send_daily_toolkit_digest():
         toolkit = Toolkit.objects.get_object_or_none(project_id=project.id)
         time_period = (timezone.localtime(timezone.now()) - timedelta(hours=settings.TOOLKIT_DIGEST_PERIOD))
         if toolkit and toolkit.modified > time_period:
-            html_template = loader.get_template("email/master-inline.html")
             for profile in project.team.all():
-                with override(profile.language):
-                    html_message = html_template.render({"type": "toolkit_digest",
-                                                         "project_id": project.id,
-                                                         "language": profile.language})
-                    subject = ugettext("Your Digital Health Atlas project has been updated")
-
-                send_mail(
-                    subject=subject,
-                    message="",
-                    from_email=settings.FROM_EMAIL,
-                    recipient_list=[profile.user.email],
-                    html_message=html_message)
+                context = {"project_id": project.id}
+                send_mail_wrapper(subject="Your Digital Health Atlas project has been updated",
+                                  email_type="toolkit_digest",
+                                  to=profile.user.email,
+                                  language=profile.language,
+                                  context=context)
