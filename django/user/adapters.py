efc5a0e1ca02ba96b08e281de0b57691da3c07ef
Gyorgy Fekete <f@pulilab.com>
2020-04-17 13:51:38 +0200

    optimize imports

--- django/user/adapters.py
+++ django/user/adapters.py
@@ -3,7 +3,6 @@ from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from allauth.account.adapter import DefaultAccountAdapter
 from rest_auth.registration.views import SocialLoginView
 from django.conf import settings
-from django.contrib.sites.models import Site
 
 from core.utils import send_mail_wrapper
 from azure.views import AzureOAuth2Adapter
@@ -14,6 +13,7 @@ import scheduler.celery # noqa
 
 
 class DefaultAccountAdapterCustom(DefaultAccountAdapter):
+    from django.contrib.sites.models import Site
     ACTIVATE_BASE_URL = 'http://' + Site.objects.get(id=settings.SITE_ID).domain + '/'
 
     def send_mail(self, template_prefix, email, context):
