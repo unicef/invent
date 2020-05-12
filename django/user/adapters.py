18766e1749b76aabb4a6a211cb43fc959037f2f1
Gyorgy Fekete <f@pulilab.com>
2020-05-12 11:12:26 +0200

    no confirmation email sent

--- django/user/adapters.py
+++ django/user/adapters.py
@@ -5,9 +5,6 @@ from allauth.account.adapter import DefaultAccountAdapter
 from django.contrib.auth import get_user_model
 from rest_auth.registration.views import SocialLoginView
 from django.conf import settings
-from django.contrib.sites.models import Site
-
-from core.utils import send_mail_wrapper
 from azure.views import AzureOAuth2Adapter
 from .models import UserProfile
 
@@ -16,19 +13,6 @@ import scheduler.celery # noqa
 
 
 class DefaultAccountAdapterCustom(DefaultAccountAdapter):
-    def send_mail(self, template_prefix, email, context):
-        ACTIVATE_BASE_URL = 'http://' + Site.objects.get(id=settings.SITE_ID).domain + '/'
-        activate_url = ACTIVATE_BASE_URL + \
-            'en/-/email-confirmation/' + context['key']
-
-        context.update({"activate_url": activate_url})
-
-        send_mail_wrapper(subject="Welcome to the T4D & Innovation Inventory Portal",
-                          email_type='email_confirmation_signup_message',
-                          to=email,
-                          language=context['user'].userprofile.language,
-                          context=context)
-
     def validate_unique_email(self, email):
         return email
 
