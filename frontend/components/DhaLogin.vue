3cea2fb3cd7630b63e473eb99d27520d7334c9e2
Alonso Lamas <al@pulilab.com>
2020-09-08 10:21:26 +0200

    Add quick fixes for presentation

--- frontend/components/DhaLogin.vue
+++ frontend/components/DhaLogin.vue
@@ -18,24 +18,15 @@
         @submit.native.prevent="loginLocal"
       >
         <fieldset>
-          <el-form-item
-            :label="$gettext('E-mail') | translate"
-            prop="username"
-          >
-            <el-input
-              v-model="username"
-              type="text"
-            />
+          <el-form-item :label="$gettext('E-mail') | translate" prop="username">
+            <el-input v-model="username" type="text" />
           </el-form-item>
 
           <el-form-item
             :label="$gettext('Password') | translate"
             prop="password"
           >
-            <el-input
-              v-model="password"
-              type="password"
-            />
+            <el-input v-model="password" type="password" />
             <div
               v-if="nonFieldErrors"
               class="el-form-item__error ModifiedFormError"
@@ -52,10 +43,7 @@
             align="middle"
             class="cardActions"
           >
-            <el-col
-              :span="6"
-              class="SecondaryAction"
-            >
+            <el-col :span="6" class="SecondaryAction">
               <el-button
                 type="text"
                 size="small"
@@ -65,15 +53,8 @@
                 <translate>Forgot password?</translate>
               </el-button>
             </el-col>
-            <el-col
-              :span="6"
-              class="PrimaryAction"
-            >
-              <el-button
-                type="primary"
-                size="medium"
-                native-type="submit"
-              >
+            <el-col :span="6" class="PrimaryAction">
+              <el-button type="primary" size="medium" native-type="submit">
                 <translate>Log in</translate>
               </el-button>
             </el-col>
@@ -92,7 +73,10 @@
       </div>
 
       <p class="Instruction">
-        <translate>Enter your email address and follow the instructions you will get by email.</translate>
+        <translate
+          >Enter your email address and follow the instructions you will get by
+          email.</translate
+        >
       </p>
 
       <el-form
@@ -104,14 +88,8 @@
         @submit.native.prevent="forgotEmail"
       >
         <fieldset>
-          <el-form-item
-            :label="$gettext('E-mail') | translate"
-            prop="email"
-          >
-            <el-input
-              v-model="email"
-              type="text"
-            />
+          <el-form-item :label="$gettext('E-mail') | translate" prop="email">
+            <el-input v-model="email" type="text" />
           </el-form-item>
         </fieldset>
 
@@ -122,10 +100,7 @@
             align="middle"
             class="cardActions"
           >
-            <el-col
-              :span="6"
-              class="SecondaryAction"
-            >
+            <el-col :span="6" class="SecondaryAction">
               <el-button
                 type="text"
                 size="small"
@@ -134,15 +109,8 @@
                 <translate>Go back to login</translate>
               </el-button>
             </el-col>
-            <el-col
-              :span="6"
-              class="PrimaryAction"
-            >
-              <el-button
-                type="primary"
-                size="medium"
-                native-type="submit"
-              >
+            <el-col :span="6" class="PrimaryAction">
+              <el-button type="primary" size="medium" native-type="submit">
                 <translate>Reset</translate>
               </el-button>
             </el-col>
@@ -162,7 +130,10 @@
       </div>
 
       <p class="Instruction">
-        <translate>An email with instructions to reset your password have been sent.</translate>
+        <translate
+          >An email with instructions to reset your password have been
+          sent.</translate
+        >
       </p>
 
       <div class="CardActionsBottom">
@@ -172,14 +143,8 @@
           align="middle"
           class="cardActions"
         >
-          <el-col
-            :span="6"
-            class="SecondaryAction"
-          />
-          <el-col
-            :span="6"
-            class="PrimaryAction"
-          >
+          <el-col :span="6" class="SecondaryAction" />
+          <el-col :span="6" class="PrimaryAction">
             <el-button
               type="primary"
               size="medium"
@@ -195,69 +160,94 @@
 </template>
 
 <script>
-import { mapActions, mapGetters } from 'vuex';
-import FormAPIErrorsMixin from './mixins/FormAPIErrorsMixin.js';
+import { mapActions, mapGetters } from "vuex";
+import FormAPIErrorsMixin from "./mixins/FormAPIErrorsMixin.js";
 
 export default {
   mixins: [FormAPIErrorsMixin],
 
-  data () {
+  data() {
     return {
-      username: '',
-      password: '',
-      email: '',
+      username: "",
+      password: "",
+      email: "",
       showForgotten: false,
       successfulReset: false,
       rules: {
         username: [
-          { required: true, message: this.$gettext('This field is required'), trigger: 'blur' },
-          { type: 'email', message: this.$gettext('Has to be a valid email address'), trigger: 'blur' },
-          { validator: this.validatorGenerator('username') }
+          {
+            required: true,
+            message: this.$gettext("This field is required"),
+            trigger: "blur"
+          },
+          {
+            type: "email",
+            message: this.$gettext("Has to be a valid email address"),
+            trigger: "blur"
+          },
+          { validator: this.validatorGenerator("username") }
         ],
         password: [
-          { required: true, message: this.$gettext('This field is required'), trigger: 'blur' },
-          { validator: this.validatorGenerator('password') }
+          {
+            required: true,
+            message: this.$gettext("This field is required"),
+            trigger: "blur"
+          },
+          { validator: this.validatorGenerator("password") }
         ]
       },
       forgettenPasswordRules: {
         email: [
-          { required: true, message: this.$gettext('This field is required'), trigger: 'blur' },
-          { type: 'email', message: this.$gettext('Has to be a valid email address'), trigger: 'blur' },
-          { validator: this.validatorGenerator('email') }
+          {
+            required: true,
+            message: this.$gettext("This field is required"),
+            trigger: "blur"
+          },
+          {
+            type: "email",
+            message: this.$gettext("Has to be a valid email address"),
+            trigger: "blur"
+          },
+          { validator: this.validatorGenerator("email") }
         ]
       }
     };
   },
   computed: {
     ...mapGetters({
-      profile: 'user/getProfile'
+      profile: "user/getProfile"
     })
   },
   methods: {
     ...mapActions({
-      login: 'user/doLogin',
-      resetPassword: 'user/resetPassword',
-      setSelectedCountry: 'dashboard/setSelectedCountry'
+      login: "user/doLogin",
+      resetPassword: "user/resetPassword",
+      setSelectedCountry: "dashboard/setSelectedCountry"
     }),
-    handleRoutingErrors (e) {
+    handleRoutingErrors(e) {
       this.$alert(
-        this.$gettext('An error occured during login, please reload the page and try again'),
-        this.$gettext('Warning'),
+        this.$gettext(
+          "An error occured during login, please reload the page and try again"
+        ),
+        this.$gettext("Warning"),
         {
-          confirmButtonText: this.$gettext('OK')
+          confirmButtonText: this.$gettext("OK")
         }
       );
       if (this.$sentry) {
-        this.$sentry.captureMessage('Un-caught validation error in project page', {
-          level: 'warning',
-          extra: {
-            e
+        this.$sentry.captureMessage(
+          "Un-caught validation error in project page",
+          {
+            level: "warning",
+            extra: {
+              e
+            }
           }
-        });
+        );
       }
     },
-    async loginLocal () {
-      this.$nuxt.$loading.start('loginLoader');
+    async loginLocal() {
+      this.$nuxt.$loading.start("loginLoader");
       this.deleteFormAPIErrors();
       try {
         await this.$refs.loginForm.validate();
@@ -270,7 +260,7 @@ export default {
           this.setFormAPIErrors(e);
           this.$refs.loginForm.validate(() => {});
         }
-        this.$nuxt.$loading.finish('loginLoader');
+        this.$nuxt.$loading.finish("loginLoader");
         return;
       }
       try {
@@ -282,20 +272,26 @@ export default {
           const query = { ...this.$route.query, next: undefined };
           this.$router.push({ path, query });
         } else {
-          this.$router.push(this.localePath({ name: 'organisation-dashboard-list', params: this.$route.params, query: { country: [this.profile.country] } }));
+          this.$router.push(
+            this.localePath({
+              name: "organisation-dashboard-list",
+              params: this.$route.params,
+              query: { country: [this.profile.country] }
+            })
+          );
         }
       } catch (e) {
         this.handleRoutingErrors(e);
       }
-      this.$nuxt.$loading.finish('loginLoader');
+      this.$nuxt.$loading.finish("loginLoader");
     },
 
-    toForgotten () {
+    toForgotten() {
       this.email = this.username;
       this.showForgotten = true;
     },
 
-    forgotEmail () {
+    forgotEmail() {
       this.deleteFormAPIErrors();
       this.$refs.forgotForm.validate(async valid => {
         if (valid) {
@@ -317,42 +313,44 @@ export default {
 </script>
 
 <style lang="less">
-  @import "../assets/style/variables.less";
-  @import "../assets/style/mixins.less";
+@import "../assets/style/variables.less";
+@import "../assets/style/mixins.less";
 
-  .LoginComponent {
-    width: @cardSizeSmall;
-    min-height: calc(100vh - @topBarHeight - @actionBarHeight - @appFooterHeight - 160px);
-    margin: 80px auto;
+.LoginComponent {
+  width: @cardSizeSmall;
+  min-height: calc(
+    100vh - @topBarHeight - @actionBarHeight - @appFooterHeight - 160px
+  );
+  margin: 80px auto;
 
-    .Instruction {
-      font-size: @fontSizeMedium;
-      line-height: 24px;
-      text-align: center;
-      color: @colorTextSecondary;
-      padding: 40px 120px 20px;
+  .Instruction {
+    font-size: @fontSizeMedium;
+    line-height: 24px;
+    text-align: center;
+    color: @colorTextSecondary;
+    padding: 40px 120px 20px;
 
-      + .el-form {
-        fieldset {
-          padding-top: 0;
-        }
-      }
-
-      + .CardActionsBottom {
-        margin-top: 40px;
+    + .el-form {
+      fieldset {
+        padding-top: 0;
       }
     }
 
-    fieldset {
-      padding: 40px 80px;
-    }
-
-    .CardActionsBottom {
-      .Separator {
-        height: 20px;
-        margin: 0 12px;
-        vertical-align: middle;
-      }
+    + .CardActionsBottom {
+      margin-top: 40px;
     }
   }
+
+  fieldset {
+    padding: 40px 80px;
+  }
+
+  .CardActionsBottom {
+    .Separator {
+      height: 20px;
+      margin: 0 12px;
+      vertical-align: middle;
+    }
+  }
+}
 </style>
