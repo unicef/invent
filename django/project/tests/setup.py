f42ee10a1aeec6c3edc6b4126c605806296ef5f9
Gyorgy Fekete <f@pulilab.com>
2020-09-11 11:44:32 +0200

    add new fields to test setup

--- django/project/tests/setup.py
+++ django/project/tests/setup.py
@@ -77,6 +77,7 @@ class SetupTests(APITestCase):
             "contact_name": "name1",
             "contact_email": "a@a.com",
             "implementation_overview": "overview",
+            "overview": "new overview",
             "implementation_dates": "2016",
             "health_focus_areas": [1, 2],
             "geographic_scope": "somewhere",
@@ -92,7 +93,13 @@ class SetupTests(APITestCase):
             "capability_levels": [],
             "capability_categories": [],
             "capability_subcategories": [],
-            "dhis": []
+            "dhis": [],
+            "unicef_sector": [1, 2],
+            "regional_priorities": [1, 2],
+            "hardware": [1, 2],
+            "nontech": [1, 2],
+            "functions": [1, 2],
+            "phase": 1,
         }}
 
         # Create project draft
