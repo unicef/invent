05dad6c7a30bdc1eed6df417828c98b4b843ead6
Gyorgy Fekete <f@pulilab.com>
2020-09-15 15:38:49 +0200

    align tests

--- django/project/tests/setup.py
+++ django/project/tests/setup.py
@@ -95,6 +95,8 @@ class SetupTests(APITestCase):
             "capability_subcategories": [],
             "dhis": [],
             "unicef_sector": [1, 2],
+            "innovation_categories": [1, 2],
+            "cpd": [1, 2],
             "regional_priorities": [1, 2],
             "hardware": [1, 2],
             "nontech": [1, 2],
