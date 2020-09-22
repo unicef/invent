30206744d553d767b94353fcd7ea4dedc22f51b1
Gyorgy Fekete <f@pulilab.com>
2020-09-22 09:03:14 +0200

    lint fix

--- django/project/tests/setup.py
+++ django/project/tests/setup.py
@@ -88,10 +88,10 @@ class TestProjectData:
             "functions": [1, 2],
             "phase": 1,
             "partners": [dict(partner_type=0, partner_name="test partner 1", partner_email="p1@partner.ppp",
-                             partner_website="https://partner1.com"),
-                        dict(partner_type=1, partner_name="test partner 2", partner_email="p2@partner.ppp",
-                             partner_website="https://partner2.com")],
-            "links": [dict(link_type=0, link_url="https://website.com"), 
+                              partner_website="https://partner1.com"),
+                         dict(partner_type=1, partner_name="test partner 2", partner_email="p2@partner.ppp",
+                              partner_website="https://partner2.com")],
+            "links": [dict(link_type=0, link_url="https://website.com"),
                       dict(link_type=1, link_url="https://sharepoint.directory")]
         }}, org, country, country_office, d1, d2
 
