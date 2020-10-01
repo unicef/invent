e321c28f2b8b346d4df77daa5129bd4ab8715474
Gyorgy Fekete <f@pulilab.com>
2020-10-01 15:07:26 +0200

    test for portfolio filters

--- django/search/tests/portfolio_tests.py
+++ django/search/tests/portfolio_tests.py
@@ -21,3 +21,29 @@ class PortfolioSearchTests(PortfolioSetup):
         response = self.user_2_client.post(url, request_data, format="json")
         self.assertEqual(response.status_code, 201, response.json())
 
+    def test_list_all_in_portfolio_for_detail_page(self):
+        url = reverse("search-project-list")
+        data = {"portfolio": self.portfolio_id, "type": "portfolio"}
+        response = self.user_2_client.get(url, data, format="json")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()['count'], 2)
+        self.assertEqual(response.json()['results']['projects'][0]['id'], self.project2_id)
+        self.assertEqual(response.json()['results']['projects'][0]['portfolio'], self.portfolio_id)
+        self.assertEqual(response.json()['results']['projects'][1]['id'], self.project4_id)
+        self.assertEqual(response.json()['results']['projects'][1]['portfolio'], self.portfolio_id)
+
+    def test_multi_portfolio_filter_should_not_work(self):
+        # add Project 3 to a Portfolio 2
+        url = reverse("portfolio-project-add", kwargs={"pk": self.portfolio2_id})
+        request_data = {"project": [self.project3_id]}
+        response = self.user_2_client.post(url, request_data, format="json")
+        self.assertEqual(response.status_code, 201, response.json())
+
+        # it only finds the last query param
+        url = reverse("search-project-list")
+        data = {"portfolio": [self.portfolio_id, self.portfolio2_id], "type": "portfolio"}
+        response = self.user_2_client.get(url, data, format="json")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()['count'], 1)
+        self.assertEqual(response.json()['results']['projects'][0]['id'], self.project3_id)
+        self.assertEqual(response.json()['results']['projects'][0]['portfolio'], self.portfolio2_id)
