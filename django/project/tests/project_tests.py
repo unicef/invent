d42d9261c4f0a886bfa4efceb11cafa298a5b2eb
Gyorgy Fekete <f@pulilab.com>
2020-12-11 16:45:30 +0100

    remove stale tests

--- django/project/tests/project_tests.py
+++ django/project/tests/project_tests.py
@@ -16,8 +16,8 @@ from rest_framework.test import APIClient
 from country.models import Country, Donor
 from project.admin import ProjectAdmin
 from user.models import Organisation, UserProfile
-from project.models import Project, DigitalStrategy, InteroperabilityLink, TechnologyPlatform, \
-    Licence, InteroperabilityStandard, HISBucket, HSCChallenge, HSCGroup, ProjectApproval
+from project.models import Project, DigitalStrategy, TechnologyPlatform, \
+    HSCChallenge, HSCGroup, ProjectApproval, Stage
 from project.tasks import send_project_approval_digest, notify_superusers_about_new_pending_approval, \
     notify_user_about_approval
 
@@ -522,25 +522,9 @@ class ProjectTests(SetupTests):
         self.assertEqual(str(ds1), '[Client] ds1')
         self.assertEqual(str(ds2), '[Client] [ds1] ds2')
 
-    def test_interop_str(self):
-        io = InteroperabilityLink.objects.create(pre='bla', name='io')
-        self.assertEqual(str(io), 'io')
-
     def test_platforms_str(self):
         tp = TechnologyPlatform.objects.create(name='tp')
         self.assertEqual(str(tp), 'tp')
-
-    def test_licences_str(self):
-        item = Licence.objects.create(name='name')
-        self.assertEqual(str(item), 'name')
-
-    def test_iopstandard_str(self):
-        item = InteroperabilityStandard.objects.create(name='name')
-        self.assertEqual(str(item), 'name')
-
-    def test_hisbucket_str(self):
-        item = HISBucket.objects.create(name='name')
-        self.assertEqual(str(item), 'name')
 
     def test_hsc_str(self):
         hsc_group = HSCGroup.objects.create(name='name')
