eaa6d2cde38584c45f416bc00fce5b9ee8c34470
Gyorgy Fekete <f@pulilab.com>
2020-12-16 15:32:28 +0100

    fix filter

--- django/project/management/commands/migrate_projects_with_old_phases.py
+++ django/project/management/commands/migrate_projects_with_old_phases.py
@@ -12,11 +12,12 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         projects = Project.objects.filter(Q(data__has_key='phase') | Q(draft__has_key='phase'))
-        conflicting_projects = Project.objects.filter(
-            (Q(data__has_key='stages') | Q(draft__has_key='stages')) & 
-            (~Q(data__stages__isnull=True) | ~Q(data__stages__exact=[]) | ~Q(data__stages__exact='') | 
-             ~Q(draft__stages__isnull=True) | ~Q(draft__stages__exact=[]) | ~Q(draft__stages__exact=''))) \
-            .filter(Q(data__has_key='phase') | Q(draft__has_key='phase'))
+        conflicting_qs = ((Q(draft__has_key='stages') & 
+                           ~Q(draft__stages__isnull=True) & ~Q(draft__stages__exact=[]) & ~Q(draft__stages__exact='')) | 
+                          (Q(data__has_key='stages') 
+                           & ~Q(data__stages__isnull=True) & ~Q(data__stages__exact=[]) & ~Q(data__stages__exact=''))) \
+                         & (Q(data__has_key='phase') | Q(draft__has_key='phase'))
+        conflicting_projects = Project.objects.filter(conflicting_qs)
         self.stdout.write(f'Migrating {projects.count()} initiatives')
         self.stdout.write(f'DEBUG IDs {list(projects.values_list("id", flat=True))}')
         self.stdout.write(f'DEBUG NAMEs {list(projects.values_list("name", flat=True))}')
