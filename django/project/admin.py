faaf0d1b338377bff8abd92346f1a86ad76f63cc
Gyorgy Fekete <f@pulilab.com>
2020-12-16 16:21:43 +0100

    fix Admin

--- django/project/admin.py
+++ django/project/admin.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
 from django.contrib.admin import SimpleListFilter
-from django.db.models import Q
 from django.utils.html import mark_safe
 from django.utils.translation import ugettext_lazy as _
 from adminsortable2.admin import SortableAdminMixin
 from core.admin import AllObjectsAdmin
-from country.models import Country
 from .models import TechnologyPlatform, DigitalStrategy, HealthFocusArea, \
     HealthCategory, HSCChallenge, Project, HSCGroup, \
     UNICEFGoal, UNICEFResultArea, UNICEFCapabilityLevel, UNICEFCapabilityCategory, \
@@ -156,9 +154,9 @@ class HSCChallengeAdmin(ViewOnlyPermissionMixin, AllObjectsAdmin):
 
 
 class ProjectAdmin(AllObjectsAdmin):
-    list_display = ['__str__', 'created', 'get_country', 'get_team', 'get_published', 'is_active']
-    readonly_fields = ['name', 'team', 'viewers', 'link', 'odk_etag', 'odk_id', 'odk_extra_data']
-    fields = ['is_active', 'name', 'team', 'viewers', 'link', 'odk_etag', 'odk_id', 'odk_extra_data']
+    list_display = ['__str__', 'modified', 'get_country', 'get_team', 'get_published', 'is_active']
+    readonly_fields = ['name', 'team', 'viewers', 'link', 'created', 'modified']
+    fields = ['is_active'] + readonly_fields
     search_fields = ['name']
 
     def get_country(self, obj):
@@ -177,17 +175,7 @@ class ProjectAdmin(AllObjectsAdmin):
     def link(self, obj):
         if obj.id is None:
             return '-'
-        version = 'publish' if obj.public_id else 'draft'
-        return mark_safe("<a target='_blank' href='/app/{}/edit-project/{}/'>See project</a>"
-                         .format(obj.id, version))
-
-    def get_queryset(self, request):
-        qs = super(ProjectAdmin, self).get_queryset(request)
-        if not request.user.is_superuser:
-            country_id_qs = Country.objects.filter(users=request.user.userprofile).values_list('id', flat=True)
-            qs = qs.filter(Q(data__country__contained_by=list(country_id_qs)) |
-                           Q(draft__country__contained_by=list(country_id_qs)))
-        return qs
+        return mark_safe(f"<a target='_blank' href='/en/-/initiatives/{obj.id}/edit/'>Edit initiative</a>")
 
     def has_add_permission(self, request):
         return False
