adf8db8165f24502fa552c273badd0c831a28991
Gyorgy Fekete <f@pulilab.com>
2020-12-18 14:13:23 +0100

    change phase migration to exclude date

--- django/project/utils.py
+++ django/project/utils.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 from typing import Dict, List, Tuple, Union
 
 from django.conf import settings
@@ -12,12 +11,11 @@ def remove_keys(data_dict: Dict, keys: Union[List, Tuple]) -> Dict:
     return data_dict
 
 
-def _migrate_phases_to_stages(data: Dict, modified: datetime):
+def _migrate_phases_to_stages(data: Dict):
     if 'phase' in data and not data.get('stages'):
         if str(data['phase']) in ID_MAP.keys():
             data["stages"] = [{
-                "id": ID_MAP[str(data['phase'])],
-                "date": modified.isoformat().replace("+00:00", "Z"),
+                "id": ID_MAP[str(data['phase'])]
             }]
         data.pop('phase')
         return True
@@ -28,6 +26,6 @@ def migrate_project_phases(project):
         if not ID_MAP or not isinstance(ID_MAP, dict):  # pragma: no cover
             print('ID MAPPING has not been completed, please run `manage.py fill_phase_mapping_table` first')
         else:
-            if any([_migrate_phases_to_stages(project.data, project.modified), 
-                    _migrate_phases_to_stages(project.draft, project.modified)]):
+            if any([_migrate_phases_to_stages(project.data), 
+                    _migrate_phases_to_stages(project.draft)]):
                 project.save(update_fields=['draft', 'data'])
