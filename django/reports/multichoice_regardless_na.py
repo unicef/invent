7149dc2446cd1f9740a366a5c1c305d9e820d0cb
Gyorgy Fekete <f@pulilab.com>
2021-01-20 16:23:10 +0100

    runscript django-extensions ready

--- django/reports/multichoice_regardless_na.py
+++ django/reports/multichoice_regardless_na.py
@@ -16,11 +16,12 @@ json_fields = [
     'draft'
 ]
 
-for p in Project.objects.all():
-    for data_or_draft in json_fields:
-        for field, klass in fields.items():
-            if field in getattr(p, data_or_draft) and len(getattr(p, data_or_draft)[field]) > 2:
-                for item in getattr(p, data_or_draft)[field]:
-                    special_ids = klass.objects.filter(name__in=special_names).values_list('id', flat=True)
-                    if item in special_ids:
-                        print(p.id, p.name, ' | ', field, data_or_draft)
+def run():
+    for p in Project.objects.all():
+        for data_or_draft in json_fields:
+            for field, klass in fields.items():
+                if field in getattr(p, data_or_draft) and len(getattr(p, data_or_draft)[field]) > 2:
+                    for item in getattr(p, data_or_draft)[field]:
+                        special_ids = klass.objects.filter(name__in=special_names).values_list('id', flat=True)
+                        if item in special_ids:
+                            print(p.id, p.name, ' | ', field, data_or_draft)
