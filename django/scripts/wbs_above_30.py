a21cbd95a3fa184971fdb6f20aeef796aef93208
Gyorgy Fekete <f@pulilab.com>
2021-01-20 16:27:36 +0100

    finalize scripts

--- django/scripts/wbs_above_30.py
+++ django/scripts/wbs_above_30.py
@@ -2,8 +2,10 @@ from project.models import Project
 
 
 def run():
+    print('Scanning for projects that have WBS longer than 30 characters')
     for p in Project.objects.all():
         if 'wbs' in p.data and len(p.data['wbs']) > 30:
             print(p.id)
         if 'wbs' in p.draft and len(p.draft['wbs']) > 30:
             print(f'draft ID: {p.id}')
+    print('Scanning finished')
