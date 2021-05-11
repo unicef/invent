f8cc94c052c2932254aaafc87fd7940ff4c3a7e6
Tibor <82872706+TiborPL@users.noreply.github.com>
2021-05-11 16:23:19 +0200

    Feature/UT03-464/copy URL in inventory list (#274)
    * Change initiative link-opening behaviour
    
    * Copy URL button in Inventory list
    
    * Fixed linting
    
    Co-authored-by: Tibor Adam <tibor.adam@gmail.com>

--- frontend/components/dashboard/DashboardFiltersHeader.vue
+++ frontend/components/dashboard/DashboardFiltersHeader.vue
@@ -5,6 +5,16 @@
         {total} initiative(s) to show
       </translate>
     </span>
+    <span v-if="canCopy" class="CopyUrl">
+      <div v-if="!urlCopied" class="Label Button" @click="copyUrlToClipboard">
+        <fa class="left" :icon="['far', 'copy']" size="lg" />
+        <translate>Copy URL</translate>
+      </div>
+      <div v-else class="Label">
+        <fa class="left" :icon="['fas', 'check-circle']" size="lg" />
+        <span>Copied</span>
+      </div>
+    </span>
     <!-- <el-button class="ToggleFiltersButton">
       <fa icon="chevron-right" />
     </el-button> -->
@@ -14,10 +24,28 @@
 <script>
 import { mapGetters } from 'vuex'
 export default {
+  data() {
+    return {
+      canCopy: false,
+      urlCopied: false,
+    }
+  },
   computed: {
     ...mapGetters({
       total: 'dashboard/getTotal',
     }),
+  },
+  mounted() {
+    this.canCopy = navigator.clipboard
+  },
+  methods: {
+    async copyUrlToClipboard() {
+      this.urlCopied = true
+      await navigator.clipboard.writeText(window.location.href)
+      setTimeout(() => {
+        this.urlCopied = false
+      }, 5000)
+    },
   },
 }
 </script>
@@ -29,18 +57,46 @@ export default {
 .DashboardFiltersHeader {
   background-color: @colorBrandPrimaryDark;
   transform: translateX(40px);
+  position: relative;
 
   .ProjectToShow {
     display: inline-block;
     box-sizing: border-box;
     padding-left: 20px;
-    width: @advancedSearchWidth - @actionBarHeight;
+    max-width: @advancedSearchWidth - @actionBarHeight;
     color: @colorWhite;
     font-size: @fontSizeBase;
     font-weight: 700;
     line-height: @actionBarHeight;
 
     .svg-inline--fa {
+      margin-right: 4px;
+    }
+  }
+
+  .CopyUrl {
+    position: absolute;
+    right: 20px;
+    top: 0;
+    bottom: 0;
+    display: inline-flex;
+    align-items: center;
+
+    .Label {
+      color: @colorWhite;
+      font-size: @fontSizeBase;
+    }
+
+    .Button {
+      cursor: pointer;
+      opacity: 0.9;
+    }
+
+    .Button:hover {
+      opacity: 1;
+    }
+
+    .left {
       margin-right: 4px;
     }
   }
