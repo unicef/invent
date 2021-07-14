fedafde2b3047c2942787fdb5729f0353be4ee23
Gyorgy Fekete <f@pulilab.com>
2021-07-14 11:38:15 +0200

    add org urls

--- django/tiip/urls.py
+++ django/tiip/urls.py
@@ -11,6 +11,7 @@ from rest_framework.routers import SimpleRouter
 
 from country.views import CountryOfficeViewSet, CountryLandingPageViewSet, CountryLandingListPageViewSet
 from project.views import ProjectPublicViewSet
+from user.views import OrganisationViewSet
 
 admin.site.site_header = 'UNICEF T4D & Innovation Inventory Portal'
 API_TITLE = 'UNICEF T4D & Innovation Inventory Portal API'
@@ -44,6 +45,7 @@ api_info_router = SimpleRouter()
 api_info_router.register(r'api/countryoffices', CountryOfficeViewSet, base_name='countryoffice')
 api_info_router.register(r'api/landing-country', CountryLandingPageViewSet, base_name='landing-country'),
 api_info_router.register(r'api/landing-country', CountryLandingListPageViewSet, base_name='landing-country'),
+api_info_router.register(r'api/organisations', OrganisationViewSet, base_name='organisation')
 
 api_info_urlpatterns = [
     url(r"^api/", include("search.urls")),
