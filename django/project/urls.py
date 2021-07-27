e135b0abc68a76e97e6177b712c62840382fac3d
David-Puli <68536648+David-Puli@users.noreply.github.com>
2021-07-27 14:50:38 +0200

    [UT03-551] Handle patch requests to ReviewScores (#297)

--- django/project/urls.py
+++ django/project/urls.py
@@ -144,6 +144,7 @@ urlpatterns = [
     path('project-review/<int:pk>/fill/',
          view=views.ReviewScoreAnswerViewSet.as_view({
              'post': 'update',
+             'patch': 'partial_update'
          }), name='review-score-fill'),
     path('project-review/manager/<int:pk>/',
          view=views.ProjectPortfolioStateManagerViewSet.as_view({
