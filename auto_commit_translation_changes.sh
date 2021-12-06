c8fbc0a15d8edb59c24d18a46a89d19864ece922
Gyorgy Fekete <f@pulilab.com>
2021-12-06 16:28:57 +0100

    update autocommit script

--- auto_commit_translation_changes.sh
+++ auto_commit_translation_changes.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash -eo pipefail
 
+# needs autotag to be installed
+# curl -O https://raw.githubusercontent.com/MichaelCurrin/auto-tag/v1.2.0/autotag
+# chmod +x autotag
+
 if git diff --quiet django/translations/master.pot; then
-   echo "No changes"
+   echo "No changes in translations"
+   exit 0
 else
    echo "Translation have been updated, we need to commit them"
+   git checkout dev
+   git pull origin dev
    git add django/translations/master.pot
-   git commit -am "update translations"
+   git commit -m "update translations [AUTO] [ci skip]"
+   ./autotag b  # increment bug version X.X.THIS
+   git push && git push --tags
+   exit 1
 fi
