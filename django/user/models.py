9dd69708c541f42e40363debd156ae011257df47
Gyorgy Fekete <f@pulilab.com>
2022-02-02 15:59:10 +0100

    remove unused import

--- django/user/models.py
+++ django/user/models.py
@@ -1,7 +1,6 @@
 from django.contrib.postgres.fields import HStoreField
 from django.db import models, transaction
 from django.conf import settings
-from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
