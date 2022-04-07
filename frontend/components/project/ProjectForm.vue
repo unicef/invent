b4de04ab3a0fecc1756dc7078d4ac3e001928176
Tibor Adam <tibor.adam@gmail.com>
2022-04-07 02:22:01 +0200

    Publish too

--- frontend/components/project/ProjectForm.vue
+++ frontend/components/project/ProjectForm.vue
@@ -298,6 +298,7 @@ export default {
               this.apiErrors = e.response.data
             } else {
               console.error(e)
+              this.setLoading(false)
             }
           }
         }
@@ -353,6 +354,7 @@ export default {
           }
         }
         this.handleErrorMessages()
+        this.setLoading(false)
       })
     },
     createdHandler() {
