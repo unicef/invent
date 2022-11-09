19ddec97dfd16351495dbaf27056636cf9052933
panagiotis.vlontzos <panagiotis.vlontzos@sword-group.com>
2022-11-09 19:30:02 +0200

    Testing to see if the unit tests will fail with the unit tests script

--- django/run_unit_tests.sh
+++ django/run_unit_tests.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 py.test --full-trace --timeout=180 --cov --cov-report html --cov-fail-under 100 --cov-report term-missing --cov-config .coveragerc 2>&1 | tee tests_output.txt
-grep "Total coverage: 100.00%" tests_output.txt
+grep "Total coverage: 1000.00%" tests_output.txt
 echo $?
