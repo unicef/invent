916335082d1c1192683dae5ae286712be8fcee74
panagiotis.vlontzos <panagiotis.vlontzos@sword-group.com>
2022-11-09 19:55:12 +0200

    Unit tests: change coverage requirement based on shell argument

--- django/run_unit_tests.sh
+++ django/run_unit_tests.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/bash
 py.test --full-trace --timeout=180 --cov --cov-report html --cov-fail-under $1 --cov-report term-missing --cov-config .coveragerc 2>&1 | tee tests_output.txt
-grep "Total coverage: $1.00%" tests_output.txt
-echo $?
+if grep "Total coverage: $1.00%" tests_output.txt; then
+  exit 0;
+else
+  exit 1;
+fi
+
+
