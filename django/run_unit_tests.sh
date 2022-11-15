3f47f6643a0643ebc4f53afc04bb21d330efdeab
panagiotis.vlontzos <panagiotis.vlontzos@sword-group.com>
2022-11-15 17:23:34 +0200

    Fixed the condition which makes the unit tests to fail

--- django/run_unit_tests.sh
+++ django/run_unit_tests.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 py.test --full-trace --timeout=180 --cov --cov-report html --cov-fail-under $1 --cov-report term-missing --cov-config .coveragerc 2>&1 | tee tests_output.txt
-if grep "Total coverage: $1.00%" tests_output.txt; then
-  exit 0;
-else
+if grep "FAIL Required" tests_output.txt; then
   exit 1;
+else
+  exit 0;
 fi
 
 
