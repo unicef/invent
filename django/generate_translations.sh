9ea8908f8064a311db4a14e9e4a717cd90c5d163
panagiotis.vlontzos <panagiotis.vlontzos@sword-group.com>
2022-12-01 15:59:11 +0200

    Added the generate translations script

---
+++ django/generate_translations.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+python manage.py update_translations master.pot
+django-admin makemessages -l en
+django-admin makemessages -l es
+django-admin makemessages -l fr
+django-admin makemessages -l pt
+python manage.py compilemessages
