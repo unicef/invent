745d2d11ef0de6724245173769e3b361077309d4
Diogenis Petropoulos <diogenis,petropoulos@sword-group.com>
2022-12-02 14:05:32 +0200

    testing lang to BE

--- frontend/lang/langReq.js
+++ frontend/lang/langReq.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 export default (ln) => {
   const base =
     process.server && process.env.NODE_ENV === 'production'
-      ? 'http://localhost:80'
+      ? 'http://invent-django:8000'
       : ''
   return axios.get(`${base}/translation/json/`, {
     headers: {
