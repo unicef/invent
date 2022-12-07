c8f2b75d924d77f283b3b1028d979deec7c0cbf1
panagiotis.vlontzos <panagiotis.vlontzos@sword-group.com>
2022-12-07 16:56:32 +0200

    Run gunicorn on the background in the startup script

--- django/startup_actions.sh
+++ django/startup_actions.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-gunicorn tiip.wsgi:application -w 2 -b :8000 --reload --timeout 120
+gunicorn tiip.wsgi:application -w 2 -b :8000 --reload --timeout 120  --daemon
 python manage.py update_translations master.pot
 django-admin makemessages -l en
 django-admin makemessages -l es
 django-admin makemessages -l fr
 django-admin makemessages -l pt
 python manage.py compilemessages
+ls -al translations/en/LC_MESSAGES
 pkill gunicorn
 gunicorn tiip.wsgi:application -w 2 -b :8000 --reload --timeout 120
