2e3cfa045b5e85e84e53b8d7c7cff39c2ce462b1
panagiotis.vlontzos <panagiotis.vlontzos@sword-group.com>
2022-12-07 15:56:16 +0200

    Revert changes to gettext import Added the restart of django to the startup script

--- django/startup_actions.sh
+++ django/startup_actions.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+gunicorn tiip.wsgi:application -w 2 -b :8000 --reload --timeout 120
 python manage.py update_translations master.pot
 django-admin makemessages -l en
 django-admin makemessages -l es
 django-admin makemessages -l fr
 django-admin makemessages -l pt
 python manage.py compilemessages
+pkill gunicorn
 gunicorn tiip.wsgi:application -w 2 -b :8000 --reload --timeout 120
