cefe1d32a165320e540dd165c6104eb18dd35bc2
panagiotis.vlontzos <panagiotis.vlontzos@sword-group.com>
2022-12-07 17:41:25 +0200

    Have persistence in the translations data

--- django/startup_actions.sh
+++ django/startup_actions.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-gunicorn tiip.wsgi:application -w 2 -b :8000 --reload --timeout 120  --daemon
+mv /tmp/pseudo_djangojs.po translations/en/LC_MESSAGES/
+mv /tmp/master.pot translations/
 python manage.py update_translations master.pot
 django-admin makemessages -l en
 django-admin makemessages -l es
 django-admin makemessages -l fr
 django-admin makemessages -l pt
 python manage.py compilemessages
-ls -al translations/en/LC_MESSAGES
-ps aux
-pkill gunicorn
 gunicorn tiip.wsgi:application -w 2 -b :8000 --reload --timeout 120
