ff10348dbc8a0d406411c2ad603022b0a0caea83
ladeniva2 <122989825+ladeniva2@users.noreply.github.com>
2023-03-23 16:11:42 +0200

    bugfix for view solution auth

--- frontend/pages/_organisation/portfolio/innovation/solutions/_id.vue
+++ frontend/pages/_organisation/portfolio/innovation/solutions/_id.vue
@@ -19,10 +19,10 @@ export default {
       handler() {
         const allowed =
           this.profile.is_superuser ||
-          this.user.global_portfolio_owner ||
-          this.user.manager.length > 0 ||
-          this.profile.member.includes(parseInt(this.$route.params.id, 10)) ||
+          this.profile.global_portfolio_owner ||
+          this.profile.manager.length > 0 ||
           this.$route.name.split('__')[0] === 'organisation-portfolio-innovation-solutions-id'
+        // this.profile.member.includes(parseInt(this.$route.params.id, 10))
         if (!allowed) {
           this.$alert(this.$gettext('You are not authorized to access this view'), this.$gettext('Warning'), {
             confirmButtonText: 'OK',
