899a476d703f4804d713f7c7c6d05e5ae8b16926
zagkliskon <124560523+zagkliskon@users.noreply.github.com>
2023-03-28 18:37:02 +0300

    Start for Innocation

--- e2e/cypress/pages/InnovationPortfoliosPage.js
+++ e2e/cypress/pages/InnovationPortfoliosPage.js
@@ -3,7 +3,18 @@
 class InnovationPortfoliosPage{
 
     getInnovationPortfoliosSection(){
-        return cy.get('class="portfolios"')
+        return cy.contains("UNICEF’s Global Innovation Portfolios")
     }
+
+    getPortfolios(){
+        return cy.get('[class="portfolio-title"]')
+    }//<span data-v-59673754="" class="portfolio-title">Climate Change</span>
+
+    openPortfolio(name){
+        this.getPortfolios().contains(name).click()
+
+    }
+
+
 }
 export default InnovationPortfoliosPage
