3e285cfa7509b411d3440b8ef0a62e8a9ce99a11
ladeniva2 <122989825+ladeniva2@users.noreply.github.com>
2023-04-07 18:49:39 +0300

    focal point and team selector new functionality

--- frontend/components/project/TeamSelector.vue
+++ frontend/components/project/TeamSelector.vue
@@ -12,15 +12,11 @@
     :value="value"
     :placeholder="$gettext('Type and select a name') | translate"
     :remote-method="filterList"
-    multiple
+    :multiple="multiple"
     filterable
     remote
     class="TeamSelector"
-    :popper-class="
-      optionsAndValues.length > value.length
-        ? 'TeamSelectorDropdown'
-        : 'NoDisplay'
-    "
+    :popper-class="optionsAndValues.length > value.length ? 'TeamSelectorDropdown' : 'NoDisplay'"
     @change="changeHandler"
     @keyup.enter.native="onEnter"
   >
@@ -29,9 +25,7 @@
       :key="person.id"
       :label="
         `${person.name ? person.name + ', ' : ''}${
-          person.organisation
-            ? getOrganisationDetails(person.organisation).name
-            : ''
+          person.organisation ? getOrganisationDetails(person.organisation).name : ''
         } ${person.name ? '(' + person.email + ')' : person.email}` | truncate
       "
       :value="person.id"
@@ -42,7 +36,7 @@
         <organisation-item :id="person.organisation" />
       </template>
       <template v-else>
-        <br/>
+        <br />
       </template>
       <span class="email"
         ><small>{{ person.email }}</small></span
@@ -82,6 +76,10 @@ export default {
     value: {
       type: Array,
       default: null,
+    },
+    multiple: {
+      type: Boolean,
+      default: true,
     },
   },
   data() {
@@ -125,17 +123,13 @@ export default {
       mails.map((email) => this.$emit('change', email))
     },
     validEmails(mails) {
-      return mails.filter(
-        (email) => this.validateEmail(email) && !this.arrIncludes(email)
-      )
+      return mails.filter((email) => this.validateEmail(email) && !this.arrIncludes(email))
     },
     arrIncludes(val) {
       return this.value.includes(val)
     },
     validateEmail(email) {
-      const valid =
-        validator.isEmail(email) &&
-        (email.endsWith('unicef.org') || email.endsWith('pulilab.com'))
+      const valid = validator.isEmail(email) && (email.endsWith('unicef.org') || email.endsWith('pulilab.com'))
       if (!valid) {
         this.invalidEmails = [...this.invalidEmails, email]
       }
---
+++ frontend/components/project/UserProfileSelector.vue
@@ -0,0 +1,171 @@
+<template>
+  <el-select
+    v-model="innerValue"
+    :multiple="multiple"
+    filterable
+    reserve-keyword
+    autocomplete
+    remote
+    clearable
+    :remote-method="filterMethod"
+    :placeholder="$gettext('Type and select a user') | translate"
+    class="UserProfileSelector"
+  >
+    <el-option v-for="item in filteredOptions" :key="item.email" :label="item.label" :value="item.email"> </el-option>
+  </el-select>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import VeeValidationMixin from '@/components/mixins/VeeValidationMixin.js'
+
+export default {
+  mixins: [VeeValidationMixin],
+  filters: {
+    truncate(str) {
+      if (str.length > 50) return `${str.substr(0, 47)}...`
+      return str
+    },
+  },
+  $_veeValidate: {
+    value() {
+      return this.value
+    },
+    events: 'change|blur',
+  },
+  model: {
+    prop: 'value',
+    event: 'change',
+  },
+  props: {
+    value: {
+      type: Array,
+      default: null,
+    },
+    multiple: {
+      type: Boolean,
+      default: true,
+    },
+    ommit: {
+      type: String,
+      default: null,
+    },
+  },
+  data() {
+    return {
+      filteredOptions: [],
+    }
+  },
+
+  computed: {
+    ...mapGetters({
+      userProfiles: 'system/getUserProfilesNoFilter',
+      getOrganisationDetails: 'system/getOrganisationDetails',
+    }),
+    innerValue: {
+      get() {
+        /**Get user ids [numArray] and translate to email array from user list, if ommit email string exist we find if user id and remove it */
+        const stringEmailsArr = this.value.map(
+          (userId) => this.userProfiles.find((profile) => profile.id === userId).email
+        )
+
+        if (this.ommit) {
+          return stringEmailsArr.filter((user) => user !== this.ommit)
+        } else {
+          return stringEmailsArr
+        }
+      },
+      set(value) {
+        /**Get email array as input and translate to userIds number array, If ommit user exist we add it back */
+
+        if (this.ommit) {
+          const emailSet = new Set([...value, this.ommit])
+          const withOmmited = [...emailSet]
+          const userIds = withOmmited.map(
+            (userEmail) => this.userProfiles.find((profile) => profile.email === userEmail).id
+          )
+          this.$emit('change', userIds)
+        } else {
+          const userIds = value.map((userEmail) => this.userProfiles.find((profile) => profile.email === userEmail).id)
+          this.$emit('change', userIds)
+        }
+      },
+    },
+    concatUserData() {
+      const data = this.userProfiles.map((user) => ({
+        ...user,
+        label: `${user.name} , ${user.email}`,
+      }))
+      if (this.ommit) {
+        return data.filter((user) => user.email !== this.ommit)
+      } else {
+        return data
+      }
+    },
+  },
+  methods: {
+    filterMethod(query) {
+      if (query) {
+        this.filteredOptions = this.concatUserData.filter(
+          (p) => this.filter(p.name ? p.name : p.email, query) || (p.email ? this.filter(p.email, query) : false)
+        )
+      } else {
+        this.filteredOptions = []
+      }
+    },
+    filter(val, query) {
+      return val.toLowerCase().startsWith(query.toLowerCase())
+    },
+  },
+}
+</script>
+
+<style lang="less">
+@import '../../assets/style/variables.less';
+@import '../../assets/style/mixins.less';
+
+.UserProfileSelector {
+  width: 100%;
+  .el-select-dropdown__item.selected {
+    // display: none;
+  }
+
+  &.el-select {
+    .el-tag {
+      &:hover {
+        background-color: white;
+        border-color: #777779;
+      }
+    }
+  }
+}
+
+.NoDisplay {
+  display: none;
+}
+
+.TeamSelectorDropdown {
+  .OrganisationItem {
+    display: inline-block;
+    margin-left: 6px;
+    font-weight: 400;
+    color: @colorGray;
+
+    &::before {
+      content: '(';
+    }
+
+    &::after {
+      content: ')';
+    }
+  }
+  li {
+    height: 50px;
+    .email {
+      float: left;
+      width: 100%;
+      margin-top: -18px;
+    }
+  }
+}
+</style>
