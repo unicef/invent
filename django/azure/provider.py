1042bc0e8b293d274062a7eda86a5cbc88160105
gkalomalos <georgios.kalomalos@sword-group.com>
2023-04-11 12:36:04 +0300

    chore/Debug aad data

--- django/azure/provider.py
+++ django/azure/provider.py
@@ -96,19 +96,20 @@ def new_users():
     # Call the get_azure_ad_users function and create new users in the Django app's database
     azure_ad_users = get_azure_ad_users()
     user_model = get_user_model()
-    print("inside new users func")
-    for azure_ad_user in azure_ad_users["value"]:
-        try:
-            user_model.objects.get(username=azure_ad_user["userPrincipalName"])
-        except user_model.DoesNotExist:
-            user = user_model.objects.create_user(
-                username=azure_ad_user["userPrincipalName"],
-                email=azure_ad_user["mail"],
-                first_name=azure_ad_user["givenName"],
-                last_name=azure_ad_user["surname"],
-            )
-            print("the user: ", user)
-            print("this is a new user: ", azure_ad_user)
+    print("azure_ad_users:", azure_ad_users)
+    print('user_model:', user_model)
+    # for azure_ad_user in azure_ad_users["value"]:
+    #     try:
+    #         user_model.objects.get(username=azure_ad_user["userPrincipalName"])
+    #     except user_model.DoesNotExist:
+    #         user = user_model.objects.create_user(
+    #             username=azure_ad_user["userPrincipalName"],
+    #             email=azure_ad_user["mail"],
+    #             first_name=azure_ad_user["givenName"],
+    #             last_name=azure_ad_user["surname"],
+    #         )
+    #         print("the user: ", user)
+    #         print("this is a new user: ", azure_ad_user)
 
-            # user.save()
-        break
+    #         # user.save()
+    #     break
