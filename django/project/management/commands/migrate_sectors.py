0cbb4fedaef4edf410221c72e4fd55e6a7bc69a8
gkalomalos <georgios.kalomalos@sword-group.com>
2023-06-09 17:07:12 +0300

    fix/Change None to empty list in case of no unicef_leading_sector

--- django/project/management/commands/migrate_sectors.py
+++ django/project/management/commands/migrate_sectors.py
@@ -9,14 +9,14 @@ class Command(BaseCommand):
         for project in Project.objects.all():
             if 'unicef_sector' in project.data:
                 project.data['unicef_leading_sector'] = project.data['unicef_sector'][0] if len(
-                    project.data['unicef_sector']) > 0 else None
-                project.data['supporting_sector'] = project.data['unicef_sector'][1:] if len(
+                    project.data['unicef_sector']) > 0 else []
+                project.data['unicef_supporting_sectors'] = project.data['unicef_sector'][1:] if len(
                     project.data['unicef_sector']) > 1 else []
                 project.save()
             if 'unicef_sector' in project.draft:
-                project.draft['unicef_supporting_sectors'] = project.draft['unicef_sector'][0] if len(
-                    project.draft['unicef_sector']) > 0 else None
-                project.draft['supporting_sector'] = project.draft['unicef_sector'][1:] if len(
+                project.draft['unicef_leading_sector'] = project.draft['unicef_sector'][0] if len(
+                    project.draft['unicef_sector']) > 0 else []
+                project.draft['unicef_supporting_sectors'] = project.draft['unicef_sector'][1:] if len(
                     project.draft['unicef_sector']) > 1 else []
                 project.save()
         self.stdout.write(self.style.SUCCESS(
