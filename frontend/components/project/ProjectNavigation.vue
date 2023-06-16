f4a721f237cf05083b41636cc54e11b8a5358a47
ladeniva2 <122989825+ladeniva2@users.noreply.github.com>
2023-06-16 13:23:00 +0300

    region access revert

--- frontend/components/project/ProjectNavigation.vue
+++ frontend/components/project/ProjectNavigation.vue
@@ -213,8 +213,7 @@ export default {
         return (
           this.user.is_superuser ||
           this.user.member.includes(this.project.id) ||
-          this.user.manager_of.includes(this.project.country_office) ||
-          this.user.region === this.project.region
+          this.user.manager_of.includes(this.project.country_office)
         )
       }
       return false
