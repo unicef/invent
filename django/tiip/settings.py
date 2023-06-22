c011d980f802d02f984b2b484786b01c1ea35b6a
gkalomalos <georgios.kalomalos@sword-group.com>
2023-06-22 18:46:47 +0300

    chore/Remove configure_azure_monitor and implement older approach to identify compatibility

--- django/tiip/settings.py
+++ django/tiip/settings.py
@@ -404,11 +404,23 @@ if ENVIRONMENT in ["dev", "tst", "uat", "prd"]:
 
 # Azure Monitor OpenTelemetry
 if ENVIRONMENT in ['dev']:
-    from azure.monitor.opentelemetry import configure_azure_monitor
+    from azure.monitor.opentelemetry.exporter import AzureMonitorTraceExporter
+    from opentelemetry import trace
+    from opentelemetry.sdk.trace.export import BatchSpanProcessor
+    from opentelemetry.sdk.trace import TracerProvider
 
     # Fetch the connection string from the environment variable
     APPLICATIONINSIGHTS_CONNECTION_STRING = os.environ.get(
         'APPLICATIONINSIGHTS_CONNECTION_STRING', default='')
 
-    if APPLICATIONINSIGHTS_CONNECTION_STRING:  # Ensure the connection string exists
-        configure_azure_monitor(APPLICATIONINSIGHTS_CONNECTION_STRING)
+    if APPLICATIONINSIGHTS_CONNECTION_STRING:
+        trace.set_tracer_provider(TracerProvider())
+        tracer = trace.get_tracer(__name__)
+
+        exporter = AzureMonitorTraceExporter(
+            connection_string=APPLICATIONINSIGHTS_CONNECTION_STRING
+        )
+
+        trace.get_tracer_provider().add_span_processor(
+            BatchSpanProcessor(exporter)
+        )
