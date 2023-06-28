a05e7e6788e9ab2cb9ad0f51e0146fe007fadf1c
mgaliatsatou <122973661+mgaliatsatou@users.noreply.github.com>
2023-06-28 15:39:50 +0300

    custom management command will now be run on deployment

--- django/kpi/management/commands/update_solutionlog.py
+++ django/kpi/management/commands/update_solutionlog.py
@@ -3,8 +3,6 @@ from kpi.models import SolutionLog
 
 
 class Command(BaseCommand):
-    '''Please run 'python manage.py update_solutionlog'
-       To be run only once in each environment. '''
     help = 'Updates solution log data for May and June 2023'
 
     def handle(self, *args, **options):
