b62df5a3d84f4af64b6cd05cc9241a27b0573f75
ladeniva2 <122989825+ladeniva2@users.noreply.github.com>
2023-08-01 16:02:06 +0300

    added link to country in initiatives published view

---
+++ frontend/components/project/OfficeLinkField.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="SimpleField">
+    <div class="Header">
+      <template v-if="header">
+        {{ header }}
+      </template>
+    </div>
+    <div class="Content">
+      <span>{{ office }}</span>
+      <nuxt-link
+        data-test="country-inventory-link"
+        :to="
+          localePath({
+            name: 'organisation',
+            params: { organisation: `${country.code}` },
+            query: { ...this.$route.query },
+          })
+        "
+      >
+        {{ country.name }}
+      </nuxt-link>
+      <span>{{ `,  ${region}` }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    header: {
+      type: String,
+      default: null,
+    },
+    office: {
+      type: String,
+      default: null,
+    },
+    country: {
+      type: Object,
+      default: null,
+    },
+    region: {
+      type: String,
+      default: null,
+    },
+  },
+}
+</script>
+
+<style></style>
