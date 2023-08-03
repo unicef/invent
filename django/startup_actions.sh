5e237f428c9993ed87e9383efbe3b0a506a86bf2
gkalomalos <georgios.kalomalos@sword-group.com>
2023-08-03 21:45:57 +0300

    feat/Add command to startup_actions script

--- django/startup_actions.sh
+++ django/startup_actions.sh
@@ -9,4 +9,5 @@ django-admin makemessages -l fr
 django-admin makemessages -l pt
 python manage.py compilemessages
 python manage.py migrate
+python manage.py restore_modified_projects_part2 # TODO: Remove once it's deployed in prd
 gunicorn tiip.wsgi:application -w 2 -b :8000 --reload --timeout 120
