106da3a7d4eb32a220348d86b7c6b63f2b97f6d8
gkalomalos <georgios.kalomalos@sword-group.com>
2023-08-17 16:56:59 +0300

    feat/Add gettext to pod to avoid issues when generating translations

--- extract_translations.sh
+++ extract_translations.sh
@@ -10,8 +10,15 @@ echo "Step 1: Fetching master.pot from the pod..."
 podName=$(kubectl get pods -l app.kubernetes.io/name=invent-frontend -o jsonpath='{.items[0].metadata.name}')
 kubectl cp ${podName}:/tmp/master.pot ./master.pot -c invent-frontend
 
-# Step 2: Move the master.pot to the desired location on the local machine
-echo "Step 2: Moving master.pot to the django/translations directory..."
-cp ./master.pot django/translations/master.pot
+# Step 2: Move the master.pot to the desired location on the local machine if it exists
+if [[ -e "./master.pot" ]]; then
+    echo "Step 2: Moving master.pot to the desired location..."
+    destination="django/translations/master.pot"
 
-echo "Master.pot has been successfully extracted and copied to src/django/translations/"
+    # Overwriting the existing file, if it exists
+    mv -f ./master.pot "$destination"
+
+    echo "Master.pot has been successfully extracted to $destination"
+else
+    echo "Error: master.pot was not found. Extraction might have failed."
+fi
