8365aaaa06599a8e55bc3fb027c2da90c89b63f8
SWORDGROUP\alafazanidi <alexandra.lafazanidi@sword-group.com>
2023-09-05 14:58:59 +0300

    correct the text in longitude

--- django/country/models.py
+++ django/country/models.py
@@ -99,7 +99,7 @@ def validate_latitude(value):
     
 def validate_longitude(value):
     if value < -180 or value > 180:
-        raise ValidationError('Latitude must be between -180 and +180.')    
+        raise ValidationError('Longitude must be between -180 and +180.')    
 
 class CountryOffice(ExtendedModel):
     REGIONS = [
