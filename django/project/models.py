27d6cc914dd79e545d7d937b53689cfd765103ef
mgaliatsatou <122973661+mgaliatsatou@users.noreply.github.com>
2023-09-05 17:42:43 +0300

    added setAside2021 and setAside2022 fields to solution model

--- django/project/models.py
+++ django/project/models.py
@@ -1128,6 +1128,8 @@ class Solution(ExtendedNameOrderedSoftDeletedModel):
         help_text="Override country based calculation", null=True, blank=True
     )
     is_active = models.BooleanField()
+    setAside2021 = models.BooleanField(default=False)
+    setAside2022 = models.BooleanField(default=False)
 
     # Properties
     @property
@@ -1253,7 +1255,9 @@ class Solution(ExtendedNameOrderedSoftDeletedModel):
             'portfolio_problem_statements': [
                 {'portfolio_id': 1, 'problem_statements': [3, 4]},
                 {'portfolio_id': 2, 'problem_statements': [4, 5]}
-            ]
+            ],
+            'setAside2021': True,
+            'setAside2022': False,
         }
         """
         # Use the Prefetch object to fetch related objects efficiently
@@ -1282,6 +1286,8 @@ class Solution(ExtendedNameOrderedSoftDeletedModel):
             "people_reached": self.people_reached,
             "country_solutions": list(country_solutions),
             "portfolio_problem_statements": self.get_portfolio_problem_statements(),
+            "setAside2021": self.setAside2021,
+            "setAside2022": self.setAside2022,
         }
 
         return data
