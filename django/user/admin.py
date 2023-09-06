00d9e9b70957eeefe5b91f84767e3281aa54cf59
gkalomalos <georgios.kalomalos@sword-group.com>
2023-09-06 11:32:27 +0300

    chore/Refactor admin register process to comply with the custom_admin_site

--- django/user/admin.py
+++ django/user/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from django.contrib.admin import ModelAdmin
 
+from core.admin.custom_admin import custom_admin_site
 from .models import Organisation
 
 
-@admin.register(Organisation)
-class OrganisationAdmin(ModelAdmin):
+class OrganisationAdmin(admin.ModelAdmin):
     pass
+
+
+custom_admin_site.register(Organisation, OrganisationAdmin)
