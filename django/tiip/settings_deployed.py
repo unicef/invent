790c0703a60d44e691cf9d02b16f062a220f15dd
gkalomalos <georgios.kalomalos@sword-group.com>
2023-09-13 16:48:45 +0300

    fix/Add comma to REST_FRAMEWORK tuple to avoid TypeError

--- django/tiip/settings_deployed.py
+++ django/tiip/settings_deployed.py
@@ -71,7 +71,7 @@ ALLOWED_HOSTS = ['*']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication'
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
