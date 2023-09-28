62f0004809e6b78757e79c993b700992328763e4
gkalomalos <georgios.kalomalos@sword-group.com>
2023-09-28 13:37:01 +0300

    chore/Add migration file 0113_alter_historicalprojectapproval_options_and_more

--- django/startup_actions.sh
+++ django/startup_actions.sh
@@ -9,5 +9,6 @@ django-admin makemessages -l fr
 django-admin makemessages -l pt
 python manage.py collectstatic --noinput
 python manage.py compilemessages
+python manage.py migrate --fake project 0113_alter_historicalprojectapproval_options_and_more || echo "Could not fake the migration, continuing anyway."
 python manage.py migrate
 gunicorn tiip.wsgi:application -w 2 -b :8000 --reload --timeout 120
